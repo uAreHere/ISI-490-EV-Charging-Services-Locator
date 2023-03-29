3cee3aef45d6e0000ef9d4b6116626c6e809eca6
e_flores <83792705+uAreHere@users.noreply.github.com>
2023-03-29 01:37:49 +0000

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang = "en">
+<head>
+	<meta charset="utf-8">
+	<meta content="IE=edge, chrome=1" http-equiv="X-UA-Compatible">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<title>EV Charging Services & Locator</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<meta name="description" content="Here I will be hosting the Capstone Web-App being developed for the Spring 2023 section of ISI-490"/>
+</head>
+
+<body class="index">
+	<div class="container bg-success my-5">
+		<div class="wrapper text-light">
+			<h1>Hello World! I am Emilio, developing the EV Charging Services & Station Locator App on this server.</h1>
+			<h4>Stay tuned for the latest developments in my EV Charging Station locator & EV Charging Services Web-App</h4>
+<br></br>
+			<h5>Below are links to Project Planning Docs & Project Repository</h5>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	 <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://3.239.216.152/ProjectPlanningDocs/ISI-490-SystemRequestProposal&FeasibilityAnalysis.pdf">System Request Proposal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://3.239.216.152/ProjectPlanningDocs/ISI-490-SoftwareRequirements.pdf">Software Requirements</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://github.com/uAreHere/ISI-490-EV-Charging-Services-Locator">Source Code</a>
+        </li>
+        <li class="nav-item">
+                            <a class="nav-link" href="/ISI-490-WEBAPP/login.php">Login</a>
+                        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+		<?php
+			echo "Hello World, by Emilio Flores"
+		?>
+
+		</div>
+	</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
