4b180b6add3ccc36e79312d3aaed3e71d4a63c5d
eflores <emilio.flores@cix.csi.cuny.edu>
2023-04-08 17:02:14 -0700

    fixed link to Homepage

--- login.php
+++ login.php
@@ -23,7 +23,7 @@
     <div class="container bg-success my-5">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/hello.php">Home</a>
+                <a class="navbar-brand" href="/ISI-490-WEBAPP/hello.php">Home</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
