4a71bdcd5b644de99c22da74f46ab76d93f6fe19
eflores <emilio.flores@cix.csi.cuny.edu>
2023-04-08 17:41:37 -0700

    created uniform navbar header & footer files

---
+++ header.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="IE=edge, chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>EV Charging Services & Locator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <meta name="description"
+        content="Here I will be hosting the Capstone Web-App being developed for the Spring 2023 section of ISI-490" />
+</head>
+
+<body class="index">
+    <div class="container bg-success my-5">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/ISI-490-WEBAPP/hello.php">Home</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Project Documents
+                            </a>
+                            <ul class="dropdown-menu">
+                            <li class="nav-item">
+                            <a class="nav-link active" aria-current="page"
+                                href="https://3.239.216.152/ProjectPlanningDocs/ISI-490-SystemRequestProposal&FeasibilityAnalysis.pdf">System
+                                Request Proposal</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page"
+                                    href="https://3.239.216.152/ProjectPlanningDocs/ISI-490-SoftwareRequirements.pdf">Software
+                                    Requirements</a>
+                            </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link"
+                                href="https://github.com/uAreHere/ISI-490-EV-Charging-Services-Locator">Source Code</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/ISI-490-WEBAPP/login.php">Login</a>
+                        </li>
+                    </ul>
+                </div>
+        </nav>
+    </div>
--- login.php
+++ login.php
@@ -1,57 +1,10 @@
 <?php
+    include_once 'header.php';
     #include("connection.php");
     $username = $_POST['user'];
     $password = $_POST['password']; 
 ?>
 
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta content="IE=edge, chrome=1" http-equiv="X-UA-Compatible">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    <title>EV Charging Services & Locator</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <meta name="description"
-        content="Here I will be hosting the Capstone Web-App being developed for the Spring 2023 section of ISI-490" />
-</head>
-
-<body class="index">
-    <div class="container bg-success my-5">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/ISI-490-WEBAPP/hello.php">Home</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page"
-                                href="https://3.239.216.152/ProjectPlanningDocs/ISI-490-SystemRequestProposal&FeasibilityAnalysis.pdf">System
-                                Request Proposal</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page"
-                                href="https://3.239.216.152/ProjectPlanningDocs/ISI-490-SoftwareRequirements.pdf">Software
-                                Requirements</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link"
-                                href="https://github.com/uAreHere/ISI-490-EV-Charging-Services-Locator">Source Code</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Login</a>
-                        </li>
-                    </ul>
-                </div>
-        </nav>
-    </div>
     <div class="container card my-5" style="width: 18rem;">
         <div class="card-body" id="form">
             <form class="px-4 py-3">
@@ -81,9 +34,6 @@
     </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
+    <?php 
+        include_once 'footer.php';
+    ?>
