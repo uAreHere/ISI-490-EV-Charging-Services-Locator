d87fc66ad9fc7dd273a537120442dfa95cb9da2d
eflores <emilio.flores@cix.csi.cuny.edu>
2023-05-20 09:53:20 -0700

    Reverted to db connection file for local deployment

--- db-connection.php
+++ db-connection.php
@@ -1,15 +1,14 @@
 <?php
-#adding changes to connect to Prof.Chun hosted site
+
 $host = "localhost";
 $dbname = "user_credentials";
-$username = "phpmyadmin";
-$password = "dmproject";
+$username = "root";
+$password = "";
 
-// $mysqli = new mysqli(hostname: $host, username:$username, password:$password, database:$dbname);
-// if($mysqli->connect_errno) {
-//     die("Connection error: " .$mysqli->connect_error);
-// }
+$mysqli = new mysqli(hostname: $host, username:$username, password:$password, database:$dbname);
 
-$mysqli = mysqli_connect($host,$username,$password,$dbname) or die ("Connection Failed");
+if($mysqli->connect_errno) {
+    die("Connection error: " .$mysqli->connect_error);
+}
 
 return $mysqli;
